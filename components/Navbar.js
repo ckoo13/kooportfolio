27ec05af98511e30f0ef0412ce0aca58bb53e30e
ckoo13 <kyungmkoo13@gmail.com>
2022-12-07 15:00:56 -0800

    first commit

---
+++ components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+export default function Navbar() {
+    return (
+        <div className='navbar-container'>
+            <div className="navbar-left">
+                <img src="/memoji.png" alt="" className="navbar-img" />
+                <p></p>
+            </div>
+            <div className="navbar-right">
+
+            </div>
+        </div>
+    )
+}
