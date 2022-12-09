1c15ae1fa143b7138e3a548a5121b60bbfe3f2fb
ckoo13 <kyungmkoo13@gmail.com>
2022-12-09 09:09:24 -0800

    hero body text

--- components/Hero.js
+++ components/Hero.js
@@ -8,7 +8,7 @@ export default function Hero() {
             <div className="hero-left">
                 <TypedText />
 
-                <p>Additional text is going to be placeed here</p>
+                <p className='hero-body-text'>I'm a recent graduate of Claremont McKenna College and just moved to New York City! I'm an Economics major turned software engineer and I'm looking to pursue a career in the technology industry through building user-focused products. My passion lies in improving the lives of people through creating seamless experiences and optimizing various aspects of daily life.</p>
             </div>
             <div className="hero-right">
                 <img src="/memoji.png" alt="" className="hero-img" />
