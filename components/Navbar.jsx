3d9643f72171065f1dd6c53be6223c25d3586e18
ckoo13 <kyungmkoo13@gmail.com>
2022-12-09 16:37:17 -0800

    toggle light dark button navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import Link from 'next/link';
 
-import { Box, Flex, Text } from '@chakra-ui/react';
+import { Box, Flex, Text, Button, useColorMode } from '@chakra-ui/react';
 
 export default function Navbar() {
+    const {colorMode, toggleColorMode} = useColorMode();
+
     return (
         <Flex align='center' justify='space-between' width='85%' mt='10' >
-            <Box fontFamily='Gotham' fontWeight='bold'>
+            <Box fontFamily='Gotham' fontWeight='bold' fontSize={28}>
                 <Link className="navbar-link navbar-name" href='/' >
                         <h1 className="navbar-link">Calvin Koo</h1>
                 </Link>
             </Box>
-            <Flex justifyContent='space-between'>
+            <Flex justifyContent='space-between' alignItems='center'>
                 <Link className="navbar-link" href='/' >
                     <Text marginX={4}>Home</Text>
                 </Link>
@@ -21,7 +23,8 @@ export default function Navbar() {
                 <Link className="navbar-link" href='/about'>
                     <Text marginX={4}>About</Text>
                 </Link>
-                    <Text marginX={4}>Resume</Text>
+                <Text marginX={4}>Resume</Text>
+                <Button onClick={toggleColorMode} marginX={4}></Button>
             </Flex>
         </Flex>
     )
