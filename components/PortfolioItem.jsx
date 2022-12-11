325516603f7fcb07cbce3c78f10e877f9292fee8
ckoo13 <kyungmkoo13@gmail.com>
2022-12-10 16:40:36 -0800

    portfolio item component template start

--- components/PortfolioItem.jsx
+++ components/PortfolioItem.jsx
@@ -1,5 +1,13 @@
+import { Box } from '@chakra-ui/react';
 import React from 'react';
 
 export default function PortfolioItem() {
-    
+    return (
+        <Box width='100%'>
+            <Box>
+                <Text>Kooflix</Text>
+                
+            </Box>
+        </Box>
+    )
 }
