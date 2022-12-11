f175469951aa44940b8822b14a99f430520292f6
ckoo13 <kyungmkoo13@gmail.com>
2022-12-11 12:31:24 -0800

    responsive hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -11,7 +11,7 @@ export default function Hero() {
             <Box width='65%' >
                 <TypedText />
 
-                <Text fontSize={{base: '14px', md:'16px', lg:'18px'}} marginX={40} marginTop={10} fontFamily='Gotham' fontWeight='normal' lineHeight={1.5}>I'm a recent graduate of Claremont McKenna College and just moved to New York City! I'm an Economics major turned software engineer and I'm looking to pursue a career in the technology industry through building user-focused products. My passion lies in improving the lives of people through creating seamless experiences and optimizing various aspects of daily life.</Text>
+                <Text fontSize={{base: '12px', md:'16px', lg:'18px'}} marginX={{base:'0', md:'20', lg:'40'}} marginTop={10} fontFamily='Gotham' fontWeight='normal' lineHeight={1.5}>I'm a recent graduate of Claremont McKenna College and just moved to New York City! I'm an Economics major turned software engineer and I'm looking to pursue a career in the technology industry through building user-focused products. My passion lies in improving the lives of people through creating seamless experiences and optimizing various aspects of daily life.</Text>
             </Box>
             <Box>
                 <Image boxSize='300px' src="/memoji.png" alt="" marginRight={20} />
