707221919912ef362b5d78987f0dd74738de84a7
ckoo13 <kyungmkoo13@gmail.com>
2022-12-12 20:18:59 -0800

    started building out drawer component for mobile navbar

--- components/MobileNavbar.jsx
+++ components/MobileNavbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Drawer, DrawerBody, DrawerFooter, DrawerHeader, DrawerOverlay, DrawerContent, Button} from '@chakra-ui/react';
+import { Drawer, DrawerBody, DrawerFooter, DrawerHeader, DrawerOverlay, DrawerContent, Button, DrawerCloseButton} from '@chakra-ui/react';
 import { useDisclosure } from '@chakra-ui/react';
 
 
@@ -7,12 +7,30 @@ export default function MobileNavbar() {
     const { isOpen, onOpen, onClose } = useDisclosure();
     const btnRef = React.useRef();
 
+    const navigationItems = () => {
+        
+    }
+
     return (
         <>
             <Button display={{base:'block', md:'none', lg:'none'}} ref={btnRef} onClick={onOpen}>
                 Open
             </Button>
-            
+            <Drawer
+                isOpen={isOpen}
+                placement='right'
+                onClose={onClose}
+                finalFocusRef={btnRef}
+            >
+                <DrawerOverlay/>
+                <DrawerContent>
+                    <DrawerCloseButton/>
+                    <DrawerHeader>Menu</DrawerHeader>
+                    <DrawerBody>
+                        
+                    </DrawerBody>
+                </DrawerContent>
+            </Drawer>
         </>
     )
 }
