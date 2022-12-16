bce05dcedf8330a2af3d8e4e762d7d1ff858ed8e
ckoo13 <kyungmkoo13@gmail.com>
2022-12-15 22:07:57 -0800

    more tech

--- utils/experience.js
+++ utils/experience.js
@@ -8,6 +8,19 @@ const AlphaSights = {
 };
 
 const Coda = {
-    
-}
+    role: 'Growth Fellow',
+    company: 'Coda',
+    duration: 'Feb 2022 - May 2022',
+    descriptions: ['One of 20 students selected in US for competitive 12-week growth fellowship program.', 
+                    'Published templates for students, campus organizations, and administration in higher education.',
+                    'Worked directly with the Coda Growth/Go-To-Market Team to ideate and implement growth strategies across campuses.']
+};
+
+const JUMP = {
+    role: 'Venture Capital Intern',
+    company: 'JUMP Investors',
+    duration: 'May 2020 - Aug 2020',
+    descriptions: ['Completed due diligence on potential investors and portfolio companies with market caps ranging from $41M to $60',
+                    'Formulated 6 business development projects to increase revenue, create marketing strategies, and source suppliers resulting in 2 successful partnerships for portfolio companies']
+};
 
