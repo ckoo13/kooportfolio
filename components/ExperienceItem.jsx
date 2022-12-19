a2803894324eed507d3885d5ecf9d4ce2b52f1be
ckoo13 <kyungmkoo13@gmail.com>
2022-12-18 16:45:14 -0800

    useState/useEffect for experience item

--- components/ExperienceItem.jsx
+++ components/ExperienceItem.jsx
@@ -1,15 +1,26 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 import { UnorderedList, ListItem } from "@chakra-ui/react";
 
 
 
 export default function ExperienceItem({company}) {
-    const []
+    const [name, setName] = useState('');
+    const [role, setRole] = useState('');
+    const [duration, setDuration] = useState('');
+    const [descriptions, setDescriptions] = useState([]);
+
+    useEffect(() => {
+        setName(company.company);
+        setRole(company.role);
+        setDuration(company.duration);
+        setDescriptions(company.descriptions);
+    });
 
     return (
         <UnorderedList>
-
+            
         </UnorderedList>
     )
 }
