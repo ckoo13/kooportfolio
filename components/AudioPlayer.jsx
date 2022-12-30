e52f23670ffac0f9f6567cf40c3a3e5165938eab
ckoo13 <kyungmkoo13@gmail.com>
2022-12-30 12:44:07 -0800

    audio player skeleton

--- components/AudioPlayer.jsx
+++ components/AudioPlayer.jsx
@@ -3,6 +3,13 @@ import { useState, useEffect, useRef } from "react";
 
 export default function AudioPlayer({tracks}) {
     const [titles, setTitles] = useState([]);
+    const [trackIndex, setTrackIndex] = useState(0);
+    const [trackProgress, setTrackProgress] = useState(0);
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    
+
+    const audioRef = useRef(new Audio())
 
     useEffect(() => {
         setTitles(tracks);
