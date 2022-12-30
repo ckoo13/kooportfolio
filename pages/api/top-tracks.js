11013ba94347486d87b68ea3f35787cb7f59729f
ckoo13 <kyungmkoo13@gmail.com>
2022-12-29 17:44:53 -0800

    organized data

--- pages/api/top-tracks.js
+++ pages/api/top-tracks.js
@@ -4,7 +4,12 @@ export default async(req, res) => {
     const response = await getTopTracks();
     const { items } = await response.json();
 
-    // const tracks = 
+    const tracks = items.slice(0,10).map((track) => ({
+        title: track.name,
+        songUrl: track.external_urls.spotify,
+        artist: track.artists.map(_artist => _artist.name).join(', '),
+        image: track.album.images[0].url
+    }));
 
-    return res.status(200).json(items);
+    return res.status(200).json(tracks);
 }
