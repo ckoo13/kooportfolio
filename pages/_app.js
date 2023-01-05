c8473d8415d128085efc00f169f734e51b37c5ba
ckoo13 <kyungmkoo13@gmail.com>
2023-01-05 11:31:22 -0800

    changed initial colormode

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,23 @@
 import '../styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return (  
     <ChakraProvider>
       <Component {...pageProps} />
+
+      <Script strategy='afterInteractive' 
+              src="https://www.googletagmanager.com/gtag/js?id=G-VTTW78MWXC" 
+      />
+
+      <Script>
+        {`window.dataLayer = window.dataLayer || []; 
+          function gtag(){dataLayer.push(arguments);} 
+          gtag('js', new Date()); 
+          gtag('config', 'G-VTTW78MWXC');
+        `}
+      </Script>
     </ChakraProvider>
   )
 }
